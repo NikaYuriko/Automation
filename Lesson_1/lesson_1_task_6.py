a68746f47a40aecef6d978733e70cf79ef49e4db
Nika Gorokhova <veronikaandreevnago@gmail.com>
2024-05-15 14:24:28 +0300

    добавлено 7 новых файлов

---
+++ Lesson_1/lesson_1_task_6.py
@@ -0,0 +1,50 @@
+def console1():
+    print("1", end="")
+
+
+def console2():
+    print("2", end="")
+
+
+def console3():
+    print("3", end="")
+
+
+def console4():
+    print("4", end="")
+
+
+def console5():
+    print("5", end="")
+
+
+def console6():
+    print("6", end="")
+
+
+def console7():
+    print("7", end="")
+
+
+def console8():
+    print("8", end="")
+
+
+def console9():
+    print("9", end="")
+
+
+def console0():
+    print("0", end="")
+
+console8()
+console8()
+console0()
+console0()
+console5()
+console5()
+console5()
+console3()
+console5()
+console3()
+console5() #Проще позвонить, чем у кого-то занимать!
