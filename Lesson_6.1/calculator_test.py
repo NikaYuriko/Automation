17b031b4ebfe4e2b44de2daad5716c3820caee67
NikaYuriko <154815792+NikaYuriko@users.noreply.github.com>
2024-07-16 13:54:09 +0300

    Update calculator_test.py

--- Lesson_6.1/calculator_test.py
+++ Lesson_6.1/calculator_test.py
@@ -1,14 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from time import sleep
 
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService
-                          (ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 
 def test_form_calculator():
@@ -19,20 +14,16 @@ def test_form_calculator():
     input_delay = driver.find_element(By.ID, "delay")
     input_delay.clear()
     input_delay.send_keys('45')
-    sleep(1)
     driver.find_element(By.XPATH, '//span[contains(text(),"7")]').click()
-    sleep(1)
     driver.find_element(By.XPATH, '//span[contains(text(),"+")]').click()
-    sleep(1)
     driver.find_element(By.XPATH, '//span[contains(text(),"8")]').click()
-    sleep(1)
     driver.find_element(By.XPATH, '//span[contains(text(),"=")]').click()
-    sleep(1)
+    
     WebDriverWait(driver, "46").until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15"))
 
     assert driver.find_element(By.CSS_SELECTOR, "div.screen").text == "15"
 
-    sleep(3)
+    
 
     driver.quit()
