6f21b14a1f203368f0f2a531ff2252b88851ee1d
NikaYuriko <154815792+NikaYuriko@users.noreply.github.com>
2024-07-18 09:39:54 +0300

    Update calculator_test.py

--- Lesson_6.1/calculator_test.py
+++ Lesson_6.1/calculator_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
